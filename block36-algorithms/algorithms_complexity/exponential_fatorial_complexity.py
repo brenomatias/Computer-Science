4a5998decdc0a1fcd9919959bbc49cee5d829a43
brenoMatias <matiasbreno19@gmail.com>
2022-08-22 10:11:16 -0300

    fatorial and exponential complexity'

---
+++ block36-algorithms/algorithms_complexity/exponential_fatorial_complexity.py
@@ -0,0 +1,7 @@
+# Essas complexidades caracterizam algoritmos que, para aumentos pequenos no tamanho da entrada, aumentam enormemente o número de operações a serem realizadas para serem executados e, consequentemente, seu tempo de execução. A relação do tempo de execução/espaço ocupado em cada caso é a seguinte:
+
+# Exponencial: 2ⁿ (O(2ⁿ));
+# Fatorial: n! (O(n!)).
+
+
+# Mas por que alguém iria escrever um algoritmo de ordem de complexidade fatorial?!"🤔  porque não há outro algoritmo conhecido que resolve o problema
