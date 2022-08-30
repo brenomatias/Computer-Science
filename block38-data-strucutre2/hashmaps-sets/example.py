a3ecf4f12026e0b31adbcaa73fc87e92769f2edd
brenoMatias <matiasbreno19@gmail.com>
2022-08-30 10:16:30 -0300

    finishing

---
+++ block38-data-strucutre2/hashmaps-sets/example.py
@@ -0,0 +1,11 @@
+# Para armazenar os dados da pessoa de forma agregada vamos criar a classe Employee:
+
+class Employee:
+    def __init__(self, id_num, name):
+        self.id_num = id_num
+        self.name = name
+
+
+class HashMap:
+    def get_address(self, id_num):
+        return id_num % 10
